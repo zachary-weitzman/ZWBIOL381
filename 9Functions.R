14cd3dd184e54d2fb5752a58b6186fa9108f5c88
Zach <zachary@weitzman.net>
2023-03-29 15:00:24 -0400

    HW9 fixs

--- 9Functions.R
+++ 9Functions.R
@@ -3,31 +3,31 @@
 # purpose: Prepare Data for Work
 # input: Dirty Data
 # output: Clean Data
-#------------------------------------------------- 
+#################################################
 clean_data<-function(x){
   x[,12]<-as.numeric(x[,12])
   x$ratio<-x[,14]/x[,12]
   return(x)
 }
-
+#-----------------------
 ##################################################
 # FUNCTION: MeanFinder
 # purpose: Calculate Mean and SD for Sim Data from Ratio
 # input: Dataframe, Species in Quotes
 # output: Vector containg Mean and SD
-#------------------------------------------------- 
+#################################################
 MeanFinder<-function(w,x){
   xm<-mean(w$ratio[w$Abbrev.==x],na.rm=T)
   xsd<-sd(w$ratio[w$Abbrev.==x],na.rm=T)
   stuff<-c(xm,xsd)
 }
-
+#-----------------------
 ##################################################
 # FUNCTION: SimCreator
 # purpose: Create Simulated Dataframe
 # input: 1st Species MeanFinder Vector, 2nd Species MeanFinder Vector, etc.
 # output: Single Conjoined Simulated Dataframe
-#------------------------------------------------- 
+#################################################
 SimCreator<-function(x,y,z){
   A<-data.frame("PELE",rnorm(127,x[1],x[2]))
   colnames(A)<-c("Species","Ratio")
@@ -37,38 +37,38 @@ SimCreator<-function(x,y,z){
   colnames(C)<-c("Species","Ratio")
   simcolony<-rbind(A,B,C)
 }
-
+#-----------------------
 ##################################################
 # FUNCTION: ANOVATOR
 # purpose: Runs ANOVA on Simulated Data and Produces Graph
 # input: Simulated Dataframe
 # output: ANOVA analysis and Boxplot
-#------------------------------------------------- 
+#################################################
 ANOVATOR<-function(x){
   sim_aov<-aov(Ratio~Species,data=x)
   print(summary(sim_aov))
   boxplot(data=x,Ratio~Species)
 }
-
+#-----------------------
 
 ##################################################
 # FUNCTION: MeanFinder2.0
 # purpose: Calculate Mean and SD for Sim Data from TotalL
 # input: Dataframe, Species in Quotes
 # output: Vector containg Mean and SD
-#------------------------------------------------- 
+#################################################
 MeanFinder2.0<-function(w,x){
   xm<-mean(w$TotalL[w$Abbrev.==x],na.rm=T)
   xsd<-sd(w$TotalL[w$Abbrev.==x],na.rm=T)
   stuff<-c(xm,xsd)
 }
-
+#-----------------------
 ##################################################
 # FUNCTION: SimCreator2.0
 # purpose: Create Simulated Dataframe
 # input: 1st Species MeanFinder Vector, 2nd Species MeanFinder Vector, etc.
 # output: Single Conjoined Simulated Dataframe
-#------------------------------------------------- 
+#################################################
 SimCreator2.0<-function(x,y,z){
   A<-data.frame("PELE",rnorm(127,x[1],x[2]))
   colnames(A)<-c("Species","Length")
@@ -78,13 +78,13 @@ SimCreator2.0<-function(x,y,z){
   colnames(C)<-c("Species","Length")
   simcolony<-rbind(A,B,C)
 }
-
+#-----------------------
 ##################################################
 # FUNCTION: ANOVATOR2.0
 # purpose: Runs ANOVA on Simulated Data and Produces Graph
 # input: Simulated Dataframe
 # output: ANOVA analysis and Boxplot
-#------------------------------------------------- 
+#################################################
 ANOVATOR2.0<-function(x){
   sim_aov<-aov(Length~Species,data=x)
   print(summary(sim_aov))
